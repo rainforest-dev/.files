6462bdbf973c16caa8d1bbf28c5a3771dd8f82b0
Rainforest Cheng <rainforestnick@gmail.com>
2021-07-01 22:03:03 +0800

    [UPDATE] move config for dependencies to config.dependencies.zsh

--- config.zsh
+++ config.zsh
@@ -1,30 +1,13 @@
-# zlib
-export LDFLAGS="$LDFLAGS -L/usr/local/opt/zlib/lib"
-export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/zlib/include"
+export HISTFILE=$HOME/.files/.zsh_history
 
-export PKG_CONFIG_PATH="/usr/local/opt/zlib/lib/pkgconfig"
-
-# bzip2
-export PATH="/usr/local/opt/bzip2/bin:$PATH"
-
-export LDFLAGS="$LDFLAGS -L/usr/local/opt/bzip2/lib"
-export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/bzip2/include"
+setopt SHARE_HISTORY
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_FIND_NO_DUPS
+setopt HIST_IGNORE_SPACE
 
 # Flutter
 export PATH="$PATH:$HOME/.flutter/bin"
 export CHROME_EXECUTABLE=/Applications/Microsoft\ Edge.app/Contents/MacOS/Microsoft\ Edge
 
-# LuaJIT
-export PATH="/usr/local/opt/luajit-openresty/bin:$PATH"
-
-export LDFLAGS="$LDFLAGS -L/usr/local/opt/luajit-openresty/lib"
-export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/luajit-openresty/include"
-
-# icu4c
-export PATH="/usr/local/opt/icu4c/bin:$PATH"
-export PATH="/usr/local/opt/icu4c/sbin:$PATH"
-
-export LDFLAGS="$LDFLAGS -L/usr/local/opt/icu4c/lib"
-export CPPFLAGS="$CPPFLAGS -I/usr/local/opt/icu4c/include"
-
-export PKG_CONFIG_PATH="/usr/local/opt/icu4c/lib/pkgconfig"
+NVIM_CONFIG=$HOME/.config/nvim
