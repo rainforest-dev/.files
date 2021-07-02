e4860f0ee737c68c009a490287ab8bf880e26e4c
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-07-02 21:00:10 +0800

    [UPDATE] add missing configs for dependencies used to build python

---
+++ config.python.zsh
@@ -0,0 +1,26 @@
+export PYENV_ROOT="$(brew --prefix pyenv)"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+# pyenv
+# eval "$(pyenv init -)"
+# pyenv-virtualenv
+if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
+
+# protect your system python from
+# polution from 3rd-party packages
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV=true
+
+# commands to override pip restriction above.
+# use `gpip` or `gpip3` to force installation of
+# a package in the global python environment
+# Never do this! It is just an escape hatch.
+gpip(){
+   PIP_REQUIRE_VIRTUALENV="" pip "$@"
+}
+gpip3(){
+   PIP_REQUIRE_VIRTUALENV="" pip3 "$@"
+}
+
+GRPC_PYTHON_BUILD_SYSTEM_OPENSSL=1
+GRPC_PYTHON_BUILD_SYSTEM_ZLIB=1
