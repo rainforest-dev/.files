c31380c4d26dda613a3ae61446aa7eecb9c61022
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-07-02 14:26:05 +0800

    [UPDATE] update config.dependencies.zsh

---
+++ config.dependencies.zsh
@@ -0,0 +1,21 @@
+# zlib
+config_dependency zlib true
+
+# bzip2
+export PATH="/usr/local/opt/bzip2/bin:$PATH"
+
+config_dependency bzip2
+
+# LuaJIT
+export PATH="/usr/local/opt/luajit-openresty/bin:$PATH"
+
+config_dependency luajit-openresty
+
+# icu4c
+export PATH="/usr/local/opt/icu4c/bin:$PATH"
+export PATH="/usr/local/opt/icu4c/sbin:$PATH"
+
+config_dependency icu4c
+
+# openblas
+config_dependency openblas true
