61c493dec8f69a3ffe68108a36d3cc275bdb557e
r08521610 <r08521610@ntu.edu.tw>
2021-10-04 20:53:09 +0800

    [UPDATE] update path configuration for centos

--- config.centos.zsh
+++ config.centos.zsh
@@ -19,10 +19,20 @@ if [ -f /opt/etc/bashrc ]; then
         . /opt/etc/bashrc
 fi
 
-if [ -d $HOME/nvim-linux64/bin ]; then
-    export PATH=$PATH:$HOME/nvim-linux64/bin
+if [ -d $HOME/node ]; then
+    export PATH=$PATH:$HOME/node/bin
+    alias node="$HOME/.node_modules/bin"
+    alias npm="$HOME/node/bin/npm"
+    alias npx="$HOME/node/bin/npx"
+fi
+# [FabioRosado | Fix worker_threads module missing | Fabio Rosado](https://fabiorosado.dev/blog/pyright-worker-threads-missing/)
+export NODE_OPTIONS=--experimental-worker
+
+if [ -d $HOME/nvim ]; then
+    export PATH=$PATH:$HOME/nvim
 fi
 
 export TMPDIR=$HOME/.tmp
+export TEMP=$TMPDIR # for Pytorch
 
 export PATH=$HOME/Repositories/config-tools/:$PATH
