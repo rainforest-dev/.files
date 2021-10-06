6341ed796585d8c10c00ff476d13620730ca50f2
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-10-06 14:24:04 +0800

    [UPDATE] change from blamer to gitsigns

--- nvim/lua/rainforest/config/git.lua
+++ nvim/lua/rainforest/config/git.lua
@@ -1,4 +1,22 @@
 local vim = vim
 
-vim.g.blamer_enabled = 1
-vim.g.blamer_delay = 500
+vim.cmd([[:highlight GitGutterAdd guifg=#a4cf69]])
+vim.cmd([[:highlight GitGutterChange guifg=#63c1e6]])
+vim.cmd([[:highlight GitGutterDelete guifg=#d74f56]]) 
+
+require 'gitsigns'.setup {
+  signs = {
+    add = { hl = 'GitGutterAdd', text = '+' },
+    change = { hl = 'GitGutterChange', text = '~' },
+    delete = { hl = 'GitGutterDelete', text = '_' },
+    topdelete = { hl = 'GitGutterDelete', text = '‾' },
+    changedelete = { hl = 'GitGutterChange', text = '~' },
+  },
+  signcolumn = true,
+  current_line_blame = true,
+  current_line_blame_opts = {
+    virt_text = true,
+    virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+    delay = 1000,
+  },
+}
