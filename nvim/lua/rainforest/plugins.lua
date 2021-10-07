5af45146ae2789033a6ab05039ecb6a803b922cf
Rainforest Cheng <r08521610@ntu.edu.tw>
2021-10-07 19:52:31 +0800

    [ADD] add nvim-colorizer.lua

--- nvim/lua/rainforest/plugins.lua
+++ nvim/lua/rainforest/plugins.lua
@@ -24,7 +24,24 @@ packer.startup(function()
 	}
 
 	-- utils
-	use 'jiangmiao/auto-pairs'
+	use({
+		"norcalli/nvim-colorizer.lua",
+		config = function()
+			require("colorizer").setup({ "*" }, {
+				RGB = true, -- #RGB hex codes
+				RRGGBB = true, -- #RRGGBB hex codes
+				names = true, -- "Name" codes like Blue
+				RRGGBBAA = true, -- #RRGGBBAA hex codes
+				rgb_fn = true, -- CSS rgb() and rgba() functions
+				hsl_fn = true, -- CSS hsl() and hsla() functions
+				css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+				css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+				-- Available modes: foreground, background
+				mode = "foreground", -- Set the display mode.
+			})
+		end,
+	})
+	use("jiangmiao/auto-pairs")
 	-- TODO: Refine key mapping
 	use 'folke/which-key.nvim'
 	--- language packs for syntax and indentation
