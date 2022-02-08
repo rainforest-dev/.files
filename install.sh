241364b1a59f8ffc88617f9f020cc308d78163ec
Rainforest Cheng <rainforestnick@gmail.com>
2022-02-08 11:22:23 +0800

    feat: add install.sh

---
+++ install.sh
@@ -0,0 +1,16 @@
+curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | zsh
+mv $HOME/.dir_colors $HOME/.dir_colors.backup
+mv $HOME/.zprofile $HOME/.zprofile.backup
+mv $HOME/.zshrc $HOME/.zshrc.backup
+mv $HOME/.zimrc $HOME/.zimrc.backup
+mv $HOME/.p10k.zsh $HOME/.p10k.zsh.backup
+ln -s $HOME/.files/nord-dircolors/src/dir_colors $HOME/.dir_colors
+ln -s $HOME/.files/.zprofile $HOME
+ln -s $HOME/.files/.zshrc $HOME
+ln -s $HOME/.files/.zimrc $HOME
+ln -s $HOME/.files/.p10k.zsh $HOME
+zimfw install
+source $HOME/.zshrc
+apt install ripgrep neovim
+mkdir -p $(dirname $NVIM_CONFIG)
+ln -s $HOME/.files/nvim $(dirname $NVIM_CONFIG)
