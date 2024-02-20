86566531fe85ee40e29f29e9cca5101ac5059408
Rainforest Cheng <rainforestnick@gmail.com>
2024-02-20 11:04:33 +0800

    install laygit by default on mac or linux

--- install.sh
+++ install.sh
@@ -2,20 +2,25 @@
 # check if zsh is installed and install it with apt
 # change default shell to zsh
 if ! [ $(command -v zsh) ]; then
-  echo "Zsh is not installed! Installing zsh..."
-  sudo apt update
-  sudo apt install zsh -y
+	echo "Zsh is not installed! Installing zsh..."
+	sudo apt update
+	sudo apt install zsh -y
 else
-  echo "Zsh is already installed!"
+	echo "Zsh is already installed!"
 fi
 sudo chsh -s $(which zsh) $USER
 SHELL=$(which zsh)
 
+if [ "$(uname)" = "Linux" ] || [ "$(uname)" = "Darwin" ]; then
+	./brew.zsh
+fi
+
 if [ "$(uname)" = "Darwin" ]; then
-  ./mac.zsh
+	./mac.zsh
 fi
 
 ./install.zsh &
 ./nvim/install.zsh &
 wait
 exec $SHELL
+
